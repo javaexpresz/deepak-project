df7b14ac37ff8a01e78aef34da6c43193698d0f0
deepakjavaworld <deepakjavaworld@fe660f40-38a1-4065-fd25-6ab45bc94f35>
2014-03-12 10:39:22 +0000

    created one class Student by deepak

---
+++ trunk/Singleton/src/com/Test/Student.java
@@ -0,0 +1,19 @@
+package com.Test;
+
+public class Student {
+int sid;
+String name;
+public int getSid() {
+	return sid;
+}
+public void setSid(int sid) {
+	this.sid = sid;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+
+}
