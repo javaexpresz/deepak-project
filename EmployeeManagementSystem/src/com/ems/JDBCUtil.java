cf390efe8f57c155196644ac6484d1b74e5950d4
deepakjavaworld <deepakjavaworld@fe660f40-38a1-4065-fd25-6ab45bc94f35>
2014-08-13 10:43:08 +0000

    ems

--- EmployeeManagementSystem/src/com/ems/JDBCUtil.java
+++ EmployeeManagementSystem/src/com/ems/JDBCUtil.java
@@ -9,13 +9,15 @@ import java.sql.SQLException;
 public class JDBCUtil {
 static{
 	try{
-		Class.forName("com.mysql.jdbc.Driver");
+		//Class.forName("com.mysql.jdbc.Driver");
+		Class.forName("oracle.jdbc.driver.OracleDriver");
 	}catch(ClassNotFoundException e){
 		System.out.println("DriverClass Not found");
 	}	
 }
 public static Connection getMySQLConnection()throws SQLException{
-	Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3307/jdbcdb", "root", "mysql");
+	Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcdb", "root", "mysql");
+	//Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "tiger");
 	return con;
 }
 public static void cleanUp(Connection con,PreparedStatement ps)throws SQLException{
